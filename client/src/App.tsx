29a3ddb5feb56c778934c91a819593a6124dd8b1
Lucas Pereira Reis <lucasreis_15_@hotmail.com>
2021-01-27 22:07:17 -0400

    refactor: add react project with vite

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <button
+            type="button"
+            onClick={() => setCount(prevState => prevState + 1)}
+          >
+            count is: {count}
+          </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
