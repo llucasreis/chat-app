729f8975b5e35083d1454bb9f7bdce4877b39a0b
llucasreis <pro.lucasreis@gmail.com>
2021-02-05 08:47:21 -0400

    feat: add chackra-ui integration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { ChakraProvider, ColorMode, ColorModeScript } from '@chakra-ui/react';
 import App from './App';
+import { theme } from './styles/theme';
+import { config } from './styles/config';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider resetCSS theme={theme}>
+      <ColorModeScript
+        initialColorMode={config.initialColorMode as ColorMode}
+      />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
