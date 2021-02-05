da54ecd34d0877f0ba89b679354d1cc557654b42
llucasreis <pro.lucasreis@gmail.com>
2021-02-05 17:03:01 -0400

    chore: add structure of chat page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Chat from './pages/Chat';
 
 import Home from './pages/Home';
 
 const App: React.FC = () => (
   <Router>
     <Route path="/" exact component={Home} />
+    <Route path="/chat" component={Chat} />
   </Router>
 );
 
