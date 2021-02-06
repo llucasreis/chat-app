aa8ad772114bbd15d4e550e9cd8dfd94301a54ed
Lucas Pereira Reis <pro.lucasreis@gmail.com>
2021-02-06 19:57:32 -0400

    feat: add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,19 @@
-import User from '@models/User';
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
+import User from '@models/User';
 import { createServer } from 'http';
 import { Server, Socket } from 'socket.io';
 
 const app = express();
+app.use(cors());
 const httpServer = createServer(app);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+    credentials: false,
+  },
+});
 const userModel = new User();
 
 interface ISocketJoinEvent {
@@ -16,9 +23,9 @@ interface ISocketJoinEvent {
 
 io.on('connection', (socket: Socket) => {
   console.log('connected');
+  console.log(`New connection with id: ${socket.id}`);
 
   socket.on('join', data => {
-    console.log(`New connection with id: ${socket.id}`)
     const { name, room } = data as ISocketJoinEvent;
     const user = userModel.addUser({ id: socket.id, name, room });
 
